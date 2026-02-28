34a249c44d99da9aa1cc8f31868fc02189ff897c
KhushChouhan <khushchouhan9680@gmail.com>
2026-02-28 17:09:17 +0530

    Update API base URL for todos endpoint

--- src/components/API.js
+++ src/components/API.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: "https://server-p3wv.onrender.com/api/v1/todos",
+  baseURL: "https://server-p3wv.onrender.com/api/v1",
 })
 
 export default API
